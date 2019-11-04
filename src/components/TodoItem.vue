fb47f6136e8633f5d86ac86297b70325acbc61b9
sergeykonik <skonik@smedialink.com>
2019-11-04 22:08:06 +0300

    add toto list

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,37 @@
+<template>
+   <div>
+       <p  v-bind:class="{ 'completed': todo.completed }">
+           <input type="checkbox" v-model="todo.completed">
+           {{ todo.title }}
+           <input type="button" v-on:click="removeItem" value="x">
+
+       </p>
+
+
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: {
+            todo: {
+                type: Object,
+                required: true
+            },
+
+        },
+        methods: {
+            removeItem() {
+                this.$parent.$emit('remove-todo', this.todo);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .completed {
+        text-decoration: line-through;
+    }
+
+</style>
